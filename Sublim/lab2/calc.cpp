cb18f86a9ad445a4d99756e11b5083eeadefa8df
EvolkovUbunta <volklikprog@gmail.com>
2017-03-06 00:09:56 +0300

    nomo

--- Sublim/lab2/calc.cpp
+++ Sublim/lab2/calc.cpp
@@ -9,36 +9,27 @@ const char operations[7] = {'+','-','*','/','^','(',')'};//Operations
 const char number[] = {'0','1','2','3','4','5','6','7','8','9'};//Number
 
 class Tokens{
-		public:
 			char   oper [LENGTH];
 			double numb [LENGTH];
 			int ent;
+		public:
+			//Добавляем на вершину стека Number
+			int tokens_push_number(double numbe){
+				numb[ent] = numbe;
+				oper[ent] = 0;
+				ent++;
+			return 0;
+			}
+			//Добавляем на вершину стека Opertations
+			int tokens_push_operations(char operat){
+				if (strchr(operations,operat) == NULL) return 1;
+				oper[ent] = operat;
+				ent++;
+				return 0;
+			}
 
-	};
+};
 
-int tokens_push_number(class Tokens * tok,double numbe){
-        tok->numb[tok->ent] = numbe;
-        tok->oper[tok->ent] = 0;
-        tok->ent++;
-        return 0;
-}
-
-int tokens_push_number_str(class Tokens * tok,const char* numbe){
-    if(strlen(number)!=0){
-        tok->numb[tok->ent] = atof(numbe);
-        tok->oper[tok->ent] = 0;
-        tok->ent++;
-        return 0;
-    }
-    return 1;
-}
-
-int tokens_push_operations(class Tokens * tok, char operat){
-    if (strchr(operations,operat) == NULL) return 1;
-    tok->oper[tok->ent] = operat;
-    tok->ent++;
-    return 0;
-}
 
 double calc(const char * str, int * status) {
 
@@ -47,11 +38,11 @@ double calc(const char * str, int * status) {
 	cout << str << endl << endl;
 	double opera[50];
 	char *numb = new char [100];
-
+	int k=0;
 	const char *d = str;
-	ob.ent=0;
 	for (int i=0; i< strlen(str);){
 		if(str[i] >= '0' && str[i] <='9'){
+			ob.tokens_push_number(atof(str+i));
 			cout << atof(str+i) << endl;
 			d = strpbrk(str+i,operations);
 			i+=d-(str+i);
@@ -59,8 +50,8 @@ double calc(const char * str, int * status) {
 		else {
 			cout<< str[i] << endl;
 			i=i+1;
+			tokens_push_operations(str[i-1]);
 		  }
-	ob.ent++;
 	}
 }
 
