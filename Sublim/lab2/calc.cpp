450a27d3340a6b88df3a1509b5d1eeaeba7dec10
EvolkovUbunta <volklikprog@gmail.com>
2017-03-05 23:46:36 +0300

    nomo

--- Sublim/lab2/calc.cpp
+++ Sublim/lab2/calc.cpp
@@ -5,6 +5,9 @@
 
 using namespace std;
 
+const char operations[7] = {'+','-','*','/','^','(',')'};//Operations
+const char number[] = {'0','1','2','3','4','5','6','7','8','9'};//Number
+
 class Tokens{
 		public:
 			char   oper [LENGTH];
@@ -13,16 +16,16 @@ class Tokens{
 
 	};
 
-int tokens_push_number(class Tokens * tok,double number){
-        tok->numb[tok->ent] = number;
+int tokens_push_number(class Tokens * tok,double numbe){
+        tok->numb[tok->ent] = numbe;
         tok->oper[tok->ent] = 0;
         tok->ent++;
         return 0;
 }
 
-int tokens_push_number_str(class Tokens * tok,const char* number){
+int tokens_push_number_str(class Tokens * tok,const char* numbe){
     if(strlen(number)!=0){
-        tok->numb[tok->ent] = atof(number);
+        tok->numb[tok->ent] = atof(numbe);
         tok->oper[tok->ent] = 0;
         tok->ent++;
         return 0;
@@ -30,13 +33,18 @@ int tokens_push_number_str(class Tokens * tok,const char* number){
     return 1;
 }
 
+int tokens_push_operations(class Tokens * tok, char operat){
+    if (strchr(operations,operat) == NULL) return 1;
+    tok->oper[tok->ent] = operat;
+    tok->ent++;
+    return 0;
+}
+
 double calc(const char * str, int * status) {
 
 	Tokens ob;
 	double result;
 	cout << str << endl << endl;
-	char operations[] = {'+','-','*','/','^','(',')'};//Operations
-	char number[] = {'0','1','2','3','4','5','6','7','8','9'};//Number
 	double opera[50];
 	char *numb = new char [100];
 
