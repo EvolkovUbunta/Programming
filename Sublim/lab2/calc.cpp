85ecd14d2dc34ede7addd53af9ed368d3361553e
EvolkovUbunta <volklikprog@gmail.com>
2017-03-07 10:01:15 +0300

    commit

--- Sublim/lab2/calc.cpp
+++ Sublim/lab2/calc.cpp
@@ -6,73 +6,102 @@
 using namespace std;
 
 const char operations[7] = {'+','-','*','/','^','(',')'};//Operations
-const char number[] = {'0','1','2','3','4','5','6','7','8','9'};//Number
+const char number[10] = {'0','1','2','3','4','5','6','7','8','9'};//Number
 
 class Tokens{
-			char   oper [LENGTH];
-			double numb [LENGTH];
-			int ent;
-		public:
+		char   oper [LENGTH];
+		double numb [LENGTH];
+		int ent;
 
-			Tokens(){
-				oper[LENGTH] = 0;
-				numb[LENGTH] = 0;
-				ent = 0;
-			}
-			//Добавляем на вершину стека Number
-			int tokens_push_number(double numbe){
-				numb[ent] = numbe;
-				oper[ent] = 0;
-				ent++;
+	public:
+
+		Tokens(){
+			oper[LENGTH] = 0;
+			numb[LENGTH] = 0;
+			ent = 0;
+		}
+		//Добавляем на вершину стека Numbe.
+		int tokens_push_number(double numbe){
+			numb[ent] = numbe;
+			oper[ent] = 0;
+			ent++;
 			return 0;
+		}
+		//Добавляем на вершину стека Opertations.
+		int tokens_push_operations(char operat){
+			if (strchr(operations,operat) == NULL) {
+				return 1;
 			}
-			//Добавляем на вершину стека Opertations
-			int tokens_push_operations(char operat){
-				if (strchr(operations,operat) == NULL) return 1;
-				oper[ent] = operat;
-				ent++;
-				return 0;
+			oper[ent] = operat;
+			ent++;
+			return 0;
+		}
+		//Операция.
+		int tokens_operations(char oper){
+			if(strchr(operations,oper) == NULL) {
+				return 1;
 			}
+			return 0;
+		}
+		//Является операцией
+		int is_operation(char oper){
+    		if (strchr(operations,oper) == NULL) {
+    			return 1;
+    		}
+    		return 0;
+		}
+
+		//Приоритер.
+		int prioryty(char more, char litle){
+			if (litle == '+' || litle == '-') return 1;
+			if (more == '^') return 1;
+			if ((litle == '*' || litle == '/') && (more == '*' || more == '/')) return 1;
+			return 0;
+		}
+		
+		int postfix_write (Tokens &Tpost){
+			Tokens Topera;
+			int i;
+			for(i=0;i < ent;i++){
+				if (oper[i]==0){
+					Tpost.tokens_push_number(numb[i]);
+					continue;
+				}
+
+				if (1){
+					Topera.tokens_push_operations(oper[i]);
+					continue;
+				}
+			}
+		}
 };
 
-/*class  Mycalc
-{
-public:
-	 Mycalc(const char * str, int * status);
-	~ Mycalc();
-	int calculation(){
-
-	}
-};
-*/
 
 double calc(const char * str, int * status) {
-//	Mycalc mycalC(str,status);
-//	mycal.calculation();
-//	return mycalC.getResault();
-	//Деление на лексемы выполненно
-	Tokens ob;
-	cout << str << endl << endl;
-	double opera[50];
-	char *numb = new char [100];
-	int k=0;
-	const char *d = str;
-	for (int i=0; i< strlen(str);){
-		if(str[i] >= '0' && str[i] <='9'){
-			ob.tokens_push_number(atof(str+i));
-			d = strpbrk(str+i,operations);
-			i+=d-(str+i);
-	}
-		else {
-			i=i+1;
-		  }
-	}
+		
+		Tokens ob;
+		double opera[50];
+		char *numb = new char [100];
+		int k=0;
+		const char *d = str;
+		for (int i=0; i< strlen(str);){
+			if(str[i] >= '0' && str[i] <='9'){
+				ob.tokens_push_number(atof(str+i));
+				d = strpbrk(str+i,operations);
+				i+=d-(str+i);
+			}
+			else {
+				ob.tokens_push_operations(str[i]);
+				i=i+1;
+			  }
+		}
+
 }
 
 int main(){
-	//Калькулятор
 	int * status;
 	const char * str = "1.333+2+(444*6+5)-17";
+	cout << str << endl << endl;
 	calc(str,status); 
 	return 0;
 }
